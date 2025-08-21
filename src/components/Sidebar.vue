89d0a95a144b35dc26c6aea9a54c2bcf19f6adf2
kmvdev <kmvdev@yandex.ru>
2025-08-21 13:13:09 +0700

    add map,add marker, add save/remove marker logic

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,52 +1,22 @@
 <script setup lang="ts">
-import Menu from 'primevue/menu'
 import { ref } from 'vue'
+import IconField from 'primevue/iconfield'
+import InputIcon from 'primevue/inputicon'
+import InputText from 'primevue/inputtext'
 
-const items = ref([
-  {
-    separator: true,
-  },
-  {
-    label: 'Инструменты',
-    items: [
-      {
-        label: 'Добавить маркер',
-        icon: 'pi pi-plus',
-      },
-      {
-        label: 'Поиск',
-        icon: 'pi pi-search',
-      },
-    ],
-  },
-  {
-    separator: true,
-  },
-])
+const isSearchLoading = ref(false)
+const searchData = ref(null)
 </script>
 
 <template>
-  <div class="card flex justify-center h-[100vh]">
-    <Menu :model="items" class="w-full md:w-60">
-      <template #start>
-        <span class="inline-flex items-center gap-1 px-2 py-2">
-          <span class="text-xl font-semibold">TEST<span class="text-primary">MAP</span></span>
-        </span>
-      </template>
-      <template #submenulabel="{ item }">
-        <span class="text-primary font-bold">{{ item.label }}</span>
-      </template>
-      <template #end>
-        <button
-          v-ripple
-          class="relative overflow-hidden w-full border-0 bg-transparent flex items-start p-2 pl-4 hover:bg-surface-100 dark:hover:bg-surface-800 rounded-none cursor-pointer transition-colors duration-200"
-        >
-          <span class="inline-flex flex-col items-start">
-            <span class="text-sm">Admin</span>
-          </span>
-        </button>
-      </template>
-    </Menu>
+  <div class="sidebar p-5">
+    <div class="card flex flex-wrap justify-center">
+      <IconField>
+        <InputIcon class="pi pi-search" />
+        <InputText v-model="searchData" placeholder="Search" />
+        <InputIcon v-if="isSearchLoading" class="pi pi-spin pi-spinner" />
+      </IconField>
+    </div>
   </div>
 </template>
 
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import Sidebar from '@/components/Sidebar.vue'
+import Map from '@/components/Map.vue'
 </script>
 
 <template>
   <main class="w-full flex justyfy-center gap-[20px]">
     <Sidebar />
-    <div>map</div>
+    <div class="h-[100vh] w-full">
+      <Map></Map>
+    </div>
   </main>
 </template>
